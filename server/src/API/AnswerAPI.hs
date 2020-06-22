eab758dacbf862327816eace7d8bbd016b8d2375
Ali Ahmed <ali.ahmed@punchcard.io>
2020-06-21 21:45:50 -0600

    update answer working:

--- server/src/API/AnswerAPI.hs
+++ server/src/API/AnswerAPI.hs
@@ -4,8 +4,6 @@
 {-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE OverloadedStrings   #-}
 
--- https://stackoverflow.com/questions/51146315/servant-queryparams-parse-error
-
 module API.AnswerAPI (answerServer, AnswerApi) where
 
 import GHC.Generics (Generic)
@@ -18,20 +16,29 @@ import Data.Text (Text(..))
 import Data.Time (getCurrentTime)
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (FromJSON, ToJSON)
-import API.DbQueries (getAnswersByQuestionId, checkQuestion, createAnswer)
+import API.DbQueries (
+  getAnswersByQuestionId, checkQuestion, createAnswer, checkAnswer, updateAnswer)
+import Control.Monad.Except (MonadError)
 
 type AnswerApi =
   "api" :> "questions" :>
     (
-      Capture "questionId" (Key Question)
-        :> "answers" :> Get '[JSON] [Entity Answer] :<|>
-      Capture "questionId" (Key Question)
-        :> "answers" :> ReqBody '[JSON] CreateAnswerRequest :> Post '[JSON] (Entity Answer)
+        Capture "questionId" (Key Question)
+            :> "answers" :> Get '[JSON] [Entity Answer]
+      :<|>
+        Capture "questionId" (Key Question)
+            :> "answers" :> ReqBody '[JSON] CreateAnswerRequest
+            :> Post '[JSON] (Entity Answer)
+      :<|>
+        Capture "questionId" (Key Question)
+            :> "answers" :> Capture "answerId" (Key Answer)
+            :> ReqBody '[JSON] UpdateAnswerRequest
+            :> Put '[JSON] Answer
     )
 
 answerServer :: ServerT AnswerApi App
 answerServer =
-  getAllAnswers :<|> postAnswer
+  getAllAnswers :<|> postAnswer :<|> putAnswer
 
 getAllAnswers :: Key Question -> App [Entity Answer]
 getAllAnswers questionId =
@@ -40,12 +47,19 @@ getAllAnswers questionId =
 
 postAnswer :: Key Question -> CreateAnswerRequest -> App (Entity Answer)
 postAnswer questionId req = do
-  question <- runDb $ checkQuestion questionId
+  question <- runDb (checkQuestion questionId) !?? err400 { errBody = "Question not found"}
   now <- liftIO getCurrentTime
-  case question of
-    Nothing -> throwError $ err400 { errBody = "Question not found"}
-    Just q ->
-      runDb $ createAnswer questionId (content req) (userId req) now
+  runDb $ createAnswer questionId (content req) (userId req) now
+
+
+putAnswer :: Key Question -> Key Answer -> UpdateAnswerRequest -> App Answer
+putAnswer questionId answerId req = do
+  _ <- runDb (checkQuestion questionId) !?? err400 { errBody = "Question not found"}
+  _ <- runDb (checkAnswer questionId answerId) !?? err400 { errBody = "Answer not found" }
+  runDb $ updateAnswer answerId (newContent req)
+
+
+---- REQUESTS ----
 
 data CreateAnswerRequest = CreateAnswerRequest
   { content :: Text
@@ -54,3 +68,13 @@ data CreateAnswerRequest = CreateAnswerRequest
 
 instance FromJSON CreateAnswerRequest
 instance ToJSON CreateAnswerRequest
+
+data UpdateAnswerRequest = UpdateAnswerRequest
+  { newContent :: Text
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON UpdateAnswerRequest
+instance ToJSON UpdateAnswerRequest
+
+(!??) :: MonadError e m => m (Maybe a) -> e -> m a
+act !?? err = act >>= maybe (throwError err) return
--- server/src/API/DbQueries.hs
+++ server/src/API/DbQueries.hs
@@ -3,49 +3,79 @@ module API.DbQueries where
 import           Database                       ( runDb
                                                 , DbQuery
                                                 )
-import           Database.Persist               ( Entity(..)
-                                                , selectList
-                                                , insertEntity
-                                                , (=.)
-                                                )
-import           Database.Persist.Class         ( get
-                                                , updateGet
-                                                , delete
-                                                , insertEntity
-                                                )
+import           Database.Persist               ( Entity )
+import qualified Database.Persist              as P
+
 import           Database.Esqueleto             ( select
                                                 , from
                                                 , where_
                                                 , (^.)
                                                 , (==.)
                                                 , val
+                                                , update
+                                                , set
+                                                , (&&.)
+                                                , (=.)
+                                                , get
+                                                , updateGet
+                                                , delete
+                                                , insertEntity
+                                                , getBy
+                                                , selectFirst
                                                 )
 import           Model
 import           Data.Text                      ( Text )
 import           Data.Time                      ( UTCTime )
+import           Data.Maybe                     ( listToMaybe )
+
+
 
 getQuestions :: DbQuery [Entity Question]
 getQuestions = select $ from $ \q -> return q
 
+
 checkQuestion :: Key Question -> DbQuery (Maybe Question)
 checkQuestion = get
 
+
 updateQuestion :: Key Question -> Text -> Text -> DbQuery Question
 updateQuestion k title content =
-  updateGet k [QuestionTitle =. title, QuestionContent =. content]
+  updateGet k [QuestionTitle P.=. title, QuestionContent P.=. content]
+
 
 deleteQuestionById :: Key Question -> DbQuery ()
-deleteQuestionById = delete
+deleteQuestionById questionId =
+  delete $ from $ \q -> where_ (q ^. QuestionId ==. val questionId)
+
 
 getAnswersByQuestionId :: Key Question -> DbQuery [Entity Answer]
 getAnswersByQuestionId questionId = select $ from $ \answer -> do
   where_ (answer ^. AnswerQuestionId ==. val questionId)
   return answer
 
+
 insertQuestion :: Question -> DbQuery (Entity Question)
 insertQuestion = insertEntity
+
 
 createAnswer
   :: Key Question -> Text -> Int -> UTCTime -> DbQuery (Entity Answer)
 createAnswer questionId content userId now =
   insertEntity (Answer questionId content userId now)
+
+
+checkAnswer :: Key Question -> Key Answer -> DbQuery (Maybe (Entity Answer))
+checkAnswer questionId answerId = fmap listToMaybe $ select $ from $ \ans -> do
+  where_
+    (   ans
+    ^.  AnswerId
+    ==. val answerId
+    &&. ans
+    ^.  AnswerQuestionId
+    ==. val questionId
+    )
+  return ans
+
+
+updateAnswer :: Key Answer -> Text -> DbQuery Answer
+updateAnswer answerId content = updateGet answerId [AnswerContent P.=. content]
