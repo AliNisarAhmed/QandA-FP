9ab212d9cab22d63aff4c8ef3b16dc25501ddc2a
Ali Ahmed <ali.ahmed@punchcard.io>
2020-07-01 13:55:50 -0600

    Add Delete Icon

---
+++ frontend/src/Icons.elm
@@ -0,0 +1,30 @@
+module Icons exposing (trash2)
+
+import Html exposing (Html)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
+
+
+svgFeatherIcon : String -> List (Svg msg) -> Html msg
+svgFeatherIcon className =
+    svg
+        [ class <| "feather feather-" ++ className
+        , fill "none"
+        , height "24"
+        , stroke "currentColor"
+        , strokeLinecap "round"
+        , strokeLinejoin "round"
+        , strokeWidth "2"
+        , viewBox "0 0 24 24"
+        , width "24"
+        ]
+
+
+trash2 : Html msg
+trash2 =
+    svgFeatherIcon "trash-2"
+        [ Svg.polyline [ points "3 6 5 6 21 6" ] []
+        , Svg.path [ d "M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" ] []
+        , Svg.line [ x1 "10", y1 "11", x2 "10", y2 "17" ] []
+        , Svg.line [ x1 "14", y1 "11", x2 "14", y2 "17" ] []
+        ]
